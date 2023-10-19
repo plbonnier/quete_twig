5ed430d6365e5eef2890e873ac33e27c6f7c9a83
plbonnier <pl_bonnier@yahoo.fr>
2023-10-19 10:36:24 +0200

    quete 3 et ajout navbar en include

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+$contactEmail = 'test@test.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
